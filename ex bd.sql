be42cf95deefd1f59a641a0119262d699a51a114
lukask028 <54475600+lukask028@users.noreply.github.com>
2019-12-07 11:32:26 -0300

    Add files via upload

---
+++ ex bd.sql
@@ -0,0 +1,10 @@
+insert into sala (andar, capacidade)
+values (1 , 40);
+
+insert into sala (andar, capacidade)
+values (1 , 35);
+
+insert into sala (andar, capacidade)
+values (2 , 70);
+
+update sala set andar = 3 where andar = 2;
